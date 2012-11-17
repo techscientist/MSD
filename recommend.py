19f82e8a2e695ff7480beb9371bdbb3c24aaadb7
Sandeep Soni <onesadpenis@gmail.com>
2012-11-17 15:25:31 -0500

    Added the k fold cross validation to training data. Added the error calculation statistics.

--- recommend.py
+++ recommend.py
@@ -58,12 +58,32 @@ def getAllUsers (usersFile):
 	users = [index + 1 for index,user in enumerate (users)];
 	return users;
 
+def difference (userHistory, partialUserHistory):
+	differenceUserHistory = dict ();
+	for user in userHistory.keys ():
+		completeUH = userHistory[user];
+		partialUH = partialUserHistory[user];
+		diffUH = list(set(completeUH) - set(partialUH));
+		differenceUserHistory[user] = diffUH;
+	return differenceUserHistory;
+
+def calculateError (diffHistory, recommendations):
+	error = 0;
+	for user in recommendations.keys ():
+		for song in recommendations[user]:
+			if str(user) in diffHistory.keys ():
+				if song not in diffHistory[str(user)]:
+					error = error + 1;
+	return error;
+
 def main ():
 	usersFile = "../Data/kaggle_users.txt";
 	dataFile = "../Data/kaggle_data.csv";
 	users = getAllUsers (usersFile);
 	fold_size = len (users)/10;
+	print fold_size;
 	allRecommendedSongs = dict();
+	"""
 	trainingUsers = users [:9*fold_size];
 	testUsers = users [9*fold_size:];
 	
@@ -79,11 +99,14 @@ def main ():
 		popularSongs = popularity (dataFile, trainingUsers);
 		userHistory = user_history (dataFile, testUsers);
 		partialUserHistory = partial_user_history (copy.deepcopy (userHistory), 0.5);
+		differenceUserHistory = difference (userHistory, partialUserHistory);
 		recommended_songs = recommendUsers (testUsers, partialUserHistory, popularSongs, 20);
+		error = calculateError (differenceUserHistory, recommended_songs);
 		allRecommendedSongs = dict (allRecommendedSongs.items() + recommended_songs.items ());
 		print fold;
-	"""
-	print (allRecommendedSongs);
+		print error;
+	
+	#print (allRecommendedSongs);
 	
 if __name__ == "__main__":
 	main ();
